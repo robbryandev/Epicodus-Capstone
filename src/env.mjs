c88b347404629e559fbc7548d73678aac012868c
robbryandev <robbryandev@gmail.com>
2023-02-17 13:29:18 -0800

    Update env schema

--- src/env.mjs
+++ src/env.mjs
@@ -19,8 +19,13 @@ const server = z.object({
     process.env.VERCEL ? z.string().min(1) : z.string().url(),
   ),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
+  GITHUB_ID: z.string(),
+  GITHUB_SECRET: z.string(),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
+  DISCORD_AUTH_LINK: z.string(),
+  TICKET_KEY: z.string(),
+  TICKET_SECRET: z.string()
 });
 
 /**
