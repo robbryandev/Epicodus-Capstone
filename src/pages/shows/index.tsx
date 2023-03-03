aa595499a31ef16a0c7fa270c1296c11ebb99acc
robbryandev <robbryandev@gmail.com>
2023-03-03 15:27:48 -0800

    Display saved shows from firebase

--- src/pages/shows/index.tsx
+++ src/pages/shows/index.tsx
@@ -1,41 +1,62 @@
 import Card from "@/components/Card"
-import { getShows, Show } from "@/utils/shows"
+import { Show } from "@/utils/shows"
 import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
+import { onSnapshot } from "firebase/firestore";
+import { collection } from "firebase/firestore"
+import {db} from "@/utils/firebase"
 
 export default function Shows() {
   const {data: session} = useSession()
   const [shows, setShows] = useState(getSaved())
+  // function getSaved() {
+  //   let result = [] as Show[]
+  //   if (typeof window != "undefined") {
+  //     const getShow = (key) => {
+  //       const id = key.replace(`saved-${session?.user.id}-`, "")
+  //       const storageShow = localStorage.getItem(`show-${id}`)
+  //       if (storageShow != null) {
+  //         const thisShow =  JSON.parse(storageShow) as Show
+  //         return thisShow
+  //       }
+  //     }
+  //     for (let i = 0; i < localStorage.length; i++) {
+  //       const key = localStorage.key(i)
+  //       if (key != null) {
+  //         if (key.startsWith(`saved-${session?.user.id}`)) {
+  //           const newShow = getShow(key)
+  //           if (typeof newShow != "undefined") {
+  //             result.push(newShow)
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return result
+  // }
   function getSaved() {
     let result = [] as Show[]
-    if (typeof window != "undefined") {
-      const getShow = (key) => {
-        const id = key.replace(`saved-${session?.user.id}-`, "")
-        const storageShow = localStorage.getItem(`show-${id}`)
-        if (storageShow != null) {
-          const thisShow =  JSON.parse(storageShow) as Show
-          return thisShow
-        }
-      }
-      for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i)
-        if (key != null) {
-          if (key.startsWith(`saved-${session?.user.id}`)) {
-            const newShow = getShow(key)
-            if (typeof newShow != "undefined") {
-              result.push(newShow)
-            }
-          }
-        }
-      }
-    }
+    
     return result
   }
   useEffect(() => {
-    setInterval(() => {
-      setShows(getSaved())
-    }, 1500)
-  }, [])
+    const unsub = onSnapshot(collection(db, `${session?.user.id}`), (snap) => {
+        const result: Show[] = [];
+        snap.forEach((show) => {
+            const data = {
+              id: show.data().id,
+              img: show.data().img,
+              artist: show.data().artist,
+              date: show.data().date,
+              href: show.data().href,
+              saved: show.data().saved
+            }
+            result.push(data)
+        })
+        setShows(result)
+    })
+    return () => unsub();
+}, [])
   return (
     <>
       <div className="shows mx-4 py-16 p-0">
