824e819aadbe0b2a5267c80a019abe69f693bea3
robbryandev <robbryandev@gmail.com>
2023-03-07 15:27:17 -0800

    Refactor shows page

--- src/pages/shows/index.tsx
+++ src/pages/shows/index.tsx
@@ -2,44 +2,80 @@ import Card from "@/components/Card";
 import { getDate, Show, showSort } from "@/utils/shows";
 import { useSession } from "next-auth/react";
 import { ReactElement, useEffect, useState } from "react";
-import { onSnapshot } from "firebase/firestore";
-import { collection } from "firebase/firestore";
+import { onSnapshot, collection } from "firebase/firestore";
 import { db } from "@/utils/firebase";
+import { useSignal } from "@preact/signals-react";
 
-function savedShows(shows): ReactElement {
+export default function Shows() {
+  const { data: session } = useSession();
+  const [upcoming, setUpcoming] = useState([] as ReactElement[]);
+  const [memories, setMemories] = useState([] as ReactElement[]);
+  const [shows, setShows] = useState([] as Show[]);
+  const [showCustom, setShowCustom] = useState(false);
   const compareDate = Date.parse(getDate({}))
-  const upcoming = [] as ReactElement[];
-  const memories = [] as ReactElement[];
-  shows.forEach((show) => {
-    if (compareDate <= Date.parse(show.date)) {
-      upcoming.push(
-        <Card
-          key={show.id}
-          id={show.id}
-          img={show.img}
-          saved={show.saved}
-          artist={show.artist}
-          date={show.date}
-          href={show.href}
-        />
-      );
-    } else {
-      memories.push(
-        <Card
-          key={show.id}
-          id={show.id}
-          img={show.img}
-          saved={show.saved}
-          artist={show.artist}
-          date={show.date}
-          href={show.href}
-        />
-      );
-    }
-  });
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, `${session?.user.id}`), (snap) => {
+      const result: Show[] = [];
+      snap.forEach((show) => {
+        const data = {
+          id: show.data().id,
+          img: show.data().img,
+          artist: show.data().artist,
+          date: show.data().date,
+          href: show.data().href,
+          saved: show.data().saved,
+        };
+        result.push(data);
+      });
+      setShows(result.sort(showSort));
+    });
+    return () => unsub();
+  }, [session?.user.id]);
+
+  useEffect(() => {
+    let newUpcoming = [] as ReactElement[]
+    let newMemories = [] as ReactElement[]
+    shows.forEach((show) => {
+      if (compareDate <= Date.parse(show.date)) {
+        newUpcoming.push(
+          <Card
+            key={show.id}
+            id={show.id}
+            img={show.img}
+            saved={show.saved}
+            artist={show.artist}
+            date={show.date}
+            href={show.href}
+          />
+        );
+      } else {
+        newMemories.push(
+          <Card
+            key={show.id}
+            id={show.id}
+            img={show.img}
+            saved={show.saved}
+            artist={show.artist}
+            date={show.date}
+            href={show.href}
+          />
+        );
+      }
+    });
+    setUpcoming(newUpcoming)
+    setMemories(newMemories)
+  }, [shows])
   return (
     <>
     <div className="shows mx-4 p-0 py-16">
+      <button className="text-txt-main text-xl rounded-md bg-background-card py-1 px-2" onClick={() => setShowCustom(Number(showCustom) == 0)}>Custom Show</button>
+      {
+        showCustom ? (
+          <div className="artist-card-back bg-background-card p-2 pb-4 text-center w-full h-auto">
+
+          </div>
+        ) : null
+      }
       <h1 className="text-center text-txt-main text-2xl my-4">Upcoming</h1>
       {
         upcoming.length > 0 ? (
@@ -67,30 +103,4 @@ function savedShows(shows): ReactElement {
     </div>
   </>
   )
-}
-
-export default function Shows() {
-  const { data: session } = useSession();
-  const [shows, setShows] = useState([] as Show[]);
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, `${session?.user.id}`), (snap) => {
-      const result: Show[] = [];
-      snap.forEach((show) => {
-        const data = {
-          id: show.data().id,
-          img: show.data().img,
-          artist: show.data().artist,
-          date: show.data().date,
-          href: show.data().href,
-          saved: show.data().saved,
-        };
-        result.push(data);
-      });
-      setShows(result.sort(showSort));
-    });
-    return () => unsub();
-  }, [session?.user.id]);
-  return (
-    savedShows(shows)
-  );
 }
